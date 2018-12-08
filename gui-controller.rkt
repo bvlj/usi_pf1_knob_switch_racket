0fa9963f9ce15b9368ed0cda1d8088afed1f5456
Joey <bevilacquajoey@gmail.com>
2018-12-08 13:31:39 +0100

    initial implementation of MicroProgram execution and memory
    Signed-off-by: Joey <bevilacquajoey@gmail.com>

--- gui-controller.rkt
+++ gui-controller.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require rackunit)
+(require 2htdp/batch-io)
 
 ; Provide
 
@@ -10,18 +11,50 @@
 (provide world)
 (provide build-world)
 
+(provide memory)
+
+(provide memory-value)
+(provide memory-value-content)
+
+(provide memory-reset)
+(provide memory-load-microprogram)
+(provide memory-get-content)
+(provide memory-dump)
+(provide memory-write)
+
+(provide assembly-pre)
+(provide assembly-after)
+(provide assembly-load)
+(provide assembly-store)
+(provide assembly-move)
+(provide assembly-alu-operation)
+(provide assembly-branch)
+(provide assembly-bzero)
+(provide assembly-bneg)
+(provide assembly-halt)
+
 (provide on-execute)
+(provide is-off?)
 (provide run-alu-operation)
+(provide get-current-memory-instruction)
+
+(provide string?->number)
+(provide string-prefix?)
+(provide get-instr-value)
+
+; STOPSHIP TODO: hide me
+(provide program-counter)
 
 ; Data
 
 ; List of available registers
 (define LIST-OF-REGISTERS (list "R0" "R1" "R2" "R3"))
 ; List of available ALU operations
-(define LIST-OF-ALU-OPERATIONS (list "A + B" "A - B" "A or B" "A and B"))
+(define LIST-OF-ALU-OPERATIONS (list "A + B" "A - B" "A or B" "A and B" "A"))
 
 ; Default status, the computer is off
 (define STATUS-OFF -1)
+(define STATUS-HALT -2)
 ; Current status of execution
 ; A status is an Number
 (define status STATUS-OFF)
@@ -49,6 +82,9 @@
   (next-status status)
   status)
 
+(define (is-off?)
+  (= status STATUS-HALT))
+
 ; run-alu-operation: Number Number Number -> Number
 ; Run the ALU operation with the 2 given inputs
 (define (run-alu-operation op a b)
@@ -57,16 +93,206 @@
     [(= op 1) (- a b)]
     [(= op 2) (bitwise-ior a b)]
     [(= op 3) (bitwise-and a b)]
+    [(= op 4) a]
     [else 0]))
 
+; Memory
+
+; A MemoryValue is a (make-memory-value MemoryExecutable MemoryContent)
+; A MemoryExecutable is a Boolean: #t if the memoryContent is an instruction, #f if it contains data
+; A MemoryContent is one of
+;     - String: Instruction
+;     - Number: Data
+(define-struct memory-value [executable content])
+
+; Memory size
+(define MEMORY-SIZE 32)
+(define MEMORY-EMPTY (make-vector MEMORY-SIZE (make-memory-value 0 0)))
+(define memory MEMORY-EMPTY)
+
+; For reading the file
+(define memory-selector 0)
+; PC
+(define program-counter 0)
+
+(define (memory-insert instruction executable position)
+  (vector-set! memory position (make-memory-value (string?->number executable) instruction)))
+
+(define (memory-reset)
+  (set! program-counter 0)
+  (set! memory-selector 0)
+  (set! memory MEMORY-EMPTY))
+
+(define (memory-write data)
+  (cond [(> memory-selector -1) (memory-insert (string?->number data) 0 memory-selector)]))
+
+(define (memory-load-microprogram path)
+  (csv-list->memory (read-csv-file path)))
+
+(define (memory-get-content position)
+  (string?->number (memory-value-content (vector-ref memory position))))
+
+; return a string containing the memory contents
+(define (memory-dump vec str count)
+  (if (= (vector-length vec) 0)
+    str
+    (memory-dump (vector-drop vec 1)
+                 (string-append str
+                                (number->string (memory-value-executable (vector-ref vec 0)))
+                                "\t"
+                                (number?->string (memory-value-content (vector-ref vec 0)))
+                                "\n")
+                 (add1 count))))
+
+; get-current-memory-instruction: -> String
+(define (get-current-memory-instruction)
+  (if (= status STATUS-HALT)
+    ; OFF
+    "HALT"
+    ; ON
+    (if (< program-counter (vector-length memory))
+      ; Valid PC
+      (let [(mem-val (vector-ref memory program-counter))]
+        (if (= (memory-value-executable mem-val) 1)
+          ; Executable
+          (memory-value-content mem-val)
+          ; Data
+          "data"))
+      ; Invalid PC
+      (on-invalid-pc))))
+
+(define (on-invalid-pc)
+  (set! status STATUS-HALT)
+  "")
+
+(define (csv-line->memory-value line)
+  (memory-insert (second line) (first line) memory-selector)
+  (set! memory-selector (add1 memory-selector)))
+
+(define (csv-list->memory list)
+  (cond
+    [(not (empty? list))
+          (csv-line->memory-value (first list))
+          (csv-list->memory (rest list))]))
+
+; Assembly interpreter
+
+; assembly-pre: ...
+;
+; Setup the (en|dis)abled circuit parts for the assembly instruction
+(define (assembly-pre alu alu-status mem-read mem-read-status mem-write
+                      mem-write-status c-bus c-bus-status)
+  (send alu set-value alu-status)
+  (send mem-read set-value mem-read-status)
+  (send mem-write set-value mem-write-status)
+  (send c-bus set-value c-bus-status))
+
+; assembly-after: Boolean -> #<void>
+; Callback for instruction execution
+(define (assembly-after has-branched)
+  (cond [(not has-branched) (set! program-counter (add1 program-counter))]))
+
+; Assembly interpreter functions set up the world so that an execution
+; results in the required action
+
+; assembly-load: Register Number MemoryBus Number -> #<void>
+;    LOAD REG MEM
+;
+; Set the memory bus to the given value and store it
+; onto the given register
+(define (assembly-load reg reg-position mem-bus mem-position)
+  ; 0 1 0 1
+  (send mem-bus set-value (number?->string (memory-get-content mem-position)))
+  (send reg set-selection reg-position))
+
+; assembly-store: Bus Number Bus Number Alu Number -> #<void>
+;    STORE REG MEM
+;
+; Store the given bus value to the given memory address
+; through its bus
+(define (assembly-store reg reg-position alu mem-position)
+  ; 1 0 1 0
+  (send reg set-selection reg-position)
+  (set! memory-selector mem-position)
+  (send alu set-selection 4))
+
+; assembly-move: Bus Number Bus Number Alu -> #<void>
+;    MOVE REG-A REG-B
+;
+; Set the destination bus to 0, sum it with the original bus value
+; and store the result to the destination bus (results in a copy of the original
+; bus value)
+(define (assembly-move a a-position c c-position alu)
+  ; 1 0 0 1
+  (send a set-selection a-position)
+  (send c set-selection c-position)
+  (send alu set-selection 4))
+
+; assembly-alu-operation ALU AluOperation BusAddr Number BusAddr Number
+;    ADD REG-A REG-B REG-C
+;    SUM REG-A REG-B REG-C
+;    OR  REG-A REG-B REG-C
+;    AND REG-A REG-B REG-C
+;
+; Set the alu operation and the bus so that the specified
+; operation is executed and store it to a given register
+(define (assembly-alu-operation alu alu-op a a-position b b-position c c-position)
+  ; 1 0 0 1
+  (send alu set-selection alu-op)
+  (send a set-selection a-position)
+  (send b set-selection b-position)
+  (send c set-selection c-position))
+
+; assembly-branch: MemPosition
+;    BRANCH MEM
+;
+; Set the program counter to the given value
+(define (assembly-branch mem-position)
+    ; 0 0 0 0
+    (set! program-counter mem-position))
+
+; assembly-bzero: Alu Register MemPosition
+;    BZERO MEM
+;
+; Set the program counter to the given value if the alu result was 0
+(define (assembly-bzero alu-result mem-position)
+  ; 0 0 0 0
+  (cond [(= (string?->number (send alu-result get-value)) 0)
+            (assembly-branch mem-position)]))
+
+(define (assembly-bneg alu-result mem-position)
+  ; 0 0 0 0
+  (cond [(< (string?->number (send alu-result get-value)) 0)
+            (assembly-branch mem-position)]))
+
+(define (assembly-halt)
+  (set! status -2))
 ; Status
 
 ; Increase the status or eventually reset it
 (define (next-status s)
   (cond
     [(= s 4) (set! status STATUS-OFF)]
-    [else (set! status (add1 s))]))
+    [(not (= s STATUS-HALT)) (set! status (add1 s))]))
 
+; Utils
+
+(define (string?->number v)
+  (if (number? v) v
+                  (if (string? v) (string->number v) 0)))
+
+(define (number?->string v)
+  (if (string? v) v
+                  (if (number? v) (number->string v) "")))
+
+(define (bool->string v)
+  (if (eqv? v #t) "true" "false"))
+
+; get-instr-value: String Number Boolean -> Number
+(define (get-instr-value inst position is-register)
+  (let [(l (string-split inst))]
+    (string->number (if (eqv? is-register #t) (substring (list-ref l position) 1 2)
+                                      (list-ref l position)))))
 
 ; Unit tests
 
@@ -94,6 +320,9 @@
            2
            "run-alu-operation and failed")
 (check-eq? (run-alu-operation 4  1  2)
+           1
+           "run-alu-operation a failed")
+(check-eq? (run-alu-operation 5  1  2)
            0
            "run-alu-operation unexpected failed")
 ; next-status
--- gui-view.rkt
+++ gui-view.rkt
@@ -11,12 +11,21 @@
                      [width 400]
                      [height 400]))
   ; Root panels
+  (define main-panel (new horizontal-panel%
+                             [parent root]))
+  ; Panel for the main UI
+  (define working-panel (new vertical-panel%
+                             [parent main-panel]))
+  ; Panel for displaying the memory contents
+  (define memory-display-panel (new panel%
+                                    [parent main-panel]))
+  ; Main UI panels
   (define top-panel (new horizontal-panel%
-                         [parent root]))
+                         [parent working-panel]))
   (define middle-panel (new horizontal-panel%
-                            [parent root]))
+                            [parent working-panel]))
   (define bottom-panel (new horizontal-panel%
-                            [parent root]))
+                            [parent working-panel]))
   ; Top panels
   (define c-bus-addr-panel (new group-box-panel%
                                 [parent top-panel]
@@ -69,10 +78,37 @@
                           [parent c-bus-addr-panel]
                           [label ""]
                           [choices LIST-OF-REGISTERS]))
+
+  ; C Bus Enabler
+  (define c-bus-enabler (new check-box%
+                             [parent c-bus-addr-panel]
+                             [label "Enable C Bus"]
+                             [value #t]))
   ; C Bus
   (define c-bus (new text-field%
                      [parent c-bus-panel]
-                     [label "C Bus"]))
+                     [label "C Bus"]
+                     [init-value "0"]))
+  ; Memory
+  (define memory-panel (new group-box-panel%
+                            [parent c-bus-panel]
+                            [label "Memory"]))
+  (define memory-write-enabler (new check-box%
+                                    [parent memory-panel]
+                                    [label "Write memory"]
+                                    [value #f]))
+  (define memory-read-enabler (new check-box%
+                                   [parent memory-panel]
+                                   [label "Read memory"]
+                                   [value #f]))
+  (define memory-bus (new text-field%
+                          [parent memory-panel]
+                          [label "Memory Bus"]
+                          [init-value "0"]))
+  (define memory-display (new message%
+                              [parent memory-display-panel]
+                              [auto-resize #t]
+                              [label "(empty)"]))
   ; ALU panels
   (define alu-ab-panel (new horizontal-panel%
                             [parent alu-panel]))
@@ -80,13 +116,19 @@
                             [parent alu-panel]))
   (define alu-c-panel (new panel%
                            [parent alu-panel]))
+  (define alu-enabler (new check-box%
+                           [parent alu-panel]
+                           [label "Enable ALU"]
+                           [value #t]))
   ; ALU AB values
   (define alu-a-value (new text-field%
                            [parent alu-ab-panel]
-                           [label "A"]))
+                           [label "A"]
+                           [init-value "0"]))
   (define alu-b-value (new text-field%
                            [parent alu-ab-panel]
-                           [label "B"]))
+                           [label "B"]
+                           [init-value "0"]))
   ; ALU operation
   (define alu-op (new choice%
                       [parent alu-op-panel]
@@ -95,28 +137,37 @@
   ; ALU C value
   (define alu-c-value (new text-field%
                            [parent alu-c-panel]
-                           [label "C"]))
+                           [label "C"]
+                           [init-value "0"]))
   ; AB Bus
   (define a-bus (new text-field%
                      [parent ab-bus-panel]
-                     [label "A Bus"]))
+                     [label "A Bus"]
+                     [init-value "0"]))
   (define b-bus (new text-field%
                      [parent ab-bus-panel]
-                     [label "B Bus"]))
+                     [label "B Bus"]
+                     [init-value "0"]))
   ; Execute
   (define exec-btn (new button%
                         [parent btns-panel]
                         [label "Execute"]
-                        [callback (lambda (b e) (exec-animate #f))]))
+                        [callback (lambda (b e) (exec-animate))]))
 
-  ; Halth
+  ; Stepper
   (define stepper-btn (new button%
                            [parent btns-panel]
                            [label "Next step"]
-                           [callback (lambda (b e) (exec-step #f))]))
+                           [callback (lambda (b e) (exec-step))]))
+
+  ; Load
+  (define load-btn (new button%
+                        [parent btns-panel]
+                        [label "Load program"]
+                        [callback (lambda (b e) (load-program))]))
 
   ; World-builder function
-  (define (exec-step _)
+  (define (exec-step)
     (let [(status (on-execute 0))]
        (cond
          [(= status 0) (set-status-0 0)]
@@ -125,23 +176,30 @@
          [(= status 3) (set-status-3 0)]
          [(= status 4) (set-status-4 0)])))
 
-  (define (exec-animate _)
+  (define (exec-animate)
     ; Disable the btns
     (send btns-panel enable #f)
     ; Start the animation
     (set-status-0 0)
-    (sleep 1)
     (set-status-1 0)
-    (sleep 1)
     (set-status-2 0)
-    (sleep 1)
     (set-status-3 0)
-    (sleep 1)
     (set-status-4 0)
-    (sleep 1)
     ; Re-enable the btns
     (send btns-panel enable #t))
 
+  (define (load-program)
+    (memory-reset)
+    (memory-load-microprogram "test.csv")
+    (exec-program)
+  )
+
+  (define (exec-program)
+    (cond [(not (is-off?))
+      (send memory-display set-label (memory-dump memory "" 0))
+      (parse-microinstruction (get-current-memory-instruction))
+      (exec-program)
+      ]))
 
   (define (set-status-0 world)
     (send a-bus set-value (get-register-value (send a-bus-addr get-selection)))
@@ -152,17 +210,22 @@
     (send alu-b-value set-value (send b-bus get-value)))
 
   (define (set-status-2 world)
-    (send alu-c-value set-value (number->string (run-alu-operation
-                                                 (send alu-op get-selection)
-                                                  (string?->number (send alu-a-value get-value))
-                                                  (string?->number (send alu-b-value get-value))))))
+    (cond
+      [(send alu-enabler get-value) (send alu-c-value set-value
+          (number->string (run-alu-operation (send alu-op get-selection)
+                                             (string?->number (send alu-a-value get-value))
+                                             (string?->number (send alu-b-value get-value)))))]))
 
   (define (set-status-3 world)
-    (send c-bus set-value (send alu-c-value get-value)))
+    (cond
+      [(send memory-read-enabler get-value) (send c-bus set-value (send memory-bus get-value))]
+      [(send alu-enabler get-value) (send c-bus set-value (send alu-c-value get-value))]
+      [(send memory-write-enabler get-value) (memory-write (send alu-c-value get-value))]))
 
   (define (set-status-4 world)
-    (let [(register (get-register (send c-bus-addr get-selection)))]
-     (send register set-value (send c-bus get-value))))
+    (cond [(send c-bus-enabler get-value)
+      (send (get-register (send c-bus-addr get-selection))
+            set-value (send c-bus get-value))]))
 
   ; Registers utils
   (define (get-register-value v)
@@ -177,9 +240,110 @@
       [(= v 3) r3-input]
       [else #f]))
 
+  (define (parse-microinstruction instr)
+    (cond
+      [(string-prefix? instr "LOAD ") (run-load instr)]
+      [(string-prefix? instr "STORE ") (run-store instr)]
+      [(string-prefix? instr "MOVE ") (run-move instr)]
+      [(string-prefix? instr "ADD ") (run-alu-op instr 0)]
+      [(string-prefix? instr "SUB ") (run-alu-op instr 1)]
+      [(string-prefix? instr "OR ") (run-alu-op instr 2)]
+      [(string-prefix? instr "AND ") (run-alu-op instr 3)]
+      [(string-prefix? instr "BRANCH ") (run-branch instr)]
+      [(string-prefix? instr "BZERO ") (run-bzero instr)]
+      [(string-prefix? instr "BNEG ") (run-bneg instr)]
+      [(string-prefix? instr "HALT") (run-halt)]
+      [else (run-nop)]))
+
+  (define (run-load instr)
+    (let [(reg-position (get-instr-value instr 1 #t))
+          (mem-position (get-instr-value instr 2 #f))]
+      (assembly-pre alu-enabler #f
+                    memory-read-enabler #t
+                    memory-write-enabler #f
+                    c-bus-enabler #t)
+      (assembly-load c-bus-addr reg-position
+                     memory-bus mem-position)
+      (assembly-after #f))
+      (exec-animate))
+
+  (define (run-store instr)
+    (let [(reg-position (get-instr-value instr 1 #t))
+          (mem-position (get-instr-value instr 2 #f))]
+      (assembly-pre alu-enabler #t
+                    memory-read-enabler #f
+                    memory-write-enabler #t
+                    c-bus-enabler #f)
+      (assembly-store c-bus-addr reg-position
+                      alu-op mem-position)
+      (assembly-after #f)
+      (memory-write (get-register-value reg-position))
+      (exec-animate)))
+
+  (define (run-move instr)
+    (let [(a-position (get-instr-value instr 1 #t))
+          (b-position (get-instr-value instr 2 #t))]
+      (assembly-pre alu-enabler #t
+                    memory-read-enabler #f
+                    memory-write-enabler #f
+                    c-bus-enabler #t)
+      (assembly-move a-bus-addr a-position
+                     b-bus-addr b-position)
+      (assembly-after #f)
+      (exec-animate)))
+
+  (define (run-alu-op instr op)
+    (let [(a-position (get-instr-value instr 1 #t))
+          (b-position (get-instr-value instr 2 #t))
+          (c-position (get-instr-value instr 3 #t))]
+      (assembly-pre alu-enabler #t
+                    memory-read-enabler #f
+                    memory-write-enabler #f
+                    c-bus-enabler #t)
+      (assembly-alu-operation alu-op op
+                              a-bus-addr a-position
+                              b-bus-addr b-position
+                              c-bus-addr c-position)
+      (assembly-after #f)
+      (exec-animate)))
+
+  (define (run-branch instr)
+    (let [(mem-position (get-instr-value instr 1 #f))
+          (alu-result (send alu-c-value get-value))]
+      (assembly-pre alu-enabler #f
+                    memory-read-enabler #f
+                    memory-write-enabler #f
+                    c-bus-enabler #f)
+      (assembly-branch mem-position)
+      (assembly-after #t)))
+
+  ;
+  (define (run-bzero instr)
+    (let [(mem-position (get-instr-value instr 1 #f))
+          (alu-result (send alu-c-value get-value))]
+      (assembly-pre alu-enabler #f
+                    memory-read-enabler #f
+                    memory-write-enabler #f
+                    c-bus-enabler #f)
+      (assembly-bzero mem-position alu-result)
+      (assembly-after #t)))
+
+  ;
+  (define (run-bneg instr)
+    (let [(mem-position (get-instr-value instr 1 #f))
+          (alu-result (send alu-c-value get-value))]
+      (assembly-pre alu-enabler #f
+                    memory-read-enabler #f
+                    memory-write-enabler #f
+                    c-bus-enabler #f)
+      (assembly-bneg mem-position alu-result)
+      (assembly-after #t)))
+
+  (define (run-nop)
+    (assembly-after #f))
+
+  (define (run-halt)
+    (assembly-halt))
+
   ; Show the window
   (send root show #t))
-
-(define (string?->number v)
-  (if (number? v) v
-      (if (string? v) (string->number v) 0)))
